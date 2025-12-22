8204a4a01f97fd263edefc134e38cdbabe72c34e
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2025-12-23 00:09:19 +0530

    Time: 6 ms (18.14%) | Memory: 12.1 MB (5.16%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,20 +1,9 @@
-#include <iostream>
-
-using namespace std;
-
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers and numbers ending in 0 (except 0 itself) cannot be palindromes
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-
-        int reversedHalf = 0;
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
-        }
-
-        // A number is a palindrome if it's equal to its reversed half
-        return x == reversedHalf || x == reversedHalf / 10;
+        if(x < 0) return false;
+        string s = to_string(x);
+        string rev = string(s.rbegin(), s.rend());
+        return s == rev;
     }
 };
